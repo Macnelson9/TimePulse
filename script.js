6d5cb0e0fe64fa3081737ba35ef70f04a3af18ba
Macnelson9 <michaelofatu@gmail.com>
2025-03-03 01:31:37 +0100

    Add JavaScript elements

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// Selected elements
+const timeLapseLinks = document.querySelectorAll('.timeLapse--link');
+const workHrs = document.querySelector('.work--summary--hrs');
+const workSpan = document.querySelector('.work--summary--previous');
+const playHrs = document.querySelector('.play--summary--hrs');
+const playSpan = document.querySelector('.play--summary--previous');
+const studyHrs = document.querySelector('.study--summary--hrs');
+const studySpan = document.querySelector('.study--summary--previous');
+const exerciseHrs = document.querySelector('.exercise--summary--hrs');
+const exerciseSpan = document.querySelector('.exercise--summary--previous');
+const socialHrs = document.querySelector('.social--summary--hrs');
+const socialSpan = document.querySelector('.social--summary--previous');
+const selfCareHrs = document.querySelector('.selfCare--summary--hrs');
+const selfCareSpan = document.querySelector('.selfCare--summary--previous');
+
+timeLapseLinks.forEach(el => {
+    el.addEventListener('click', function () {
+        alert(`${el.textContent} was clicked!!`);
+    });
+});
